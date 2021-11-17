53ceab3727066cf2130617606fc08487f0b32729
GiovanniMealla <93947588+GiovanniMealla@users.noreply.github.com>
2021-11-17 16:20:49 -0300

    Update App.py

--- App.py
+++ App.py
@@ -4,17 +4,17 @@ App = Flask(__name__)
 @App.route("/")
 def index():
     return render_template("index.html")
-
+#xd
 @App.route("/Game")
 def index2():
     return "<h1>Game</h1>"
-
+#xd
 @App.route("/Name/<string:nombre>")
 def Name(nombre):
     return "<h1>Hola {nombre}</h1>".format
-
+#xd
 if __name__ == "__main__":
     App.run(debug=True)
     
     
- # hola buenas xd xdxd
+ # hola buenas xdxdxd
