8a8fe648d73c55cd9a1fddd985a33e2e0e18f1ac
GiovanniMealla <93947588+GiovanniMealla@users.noreply.github.com>
2021-12-05 11:50:42 -0300

    Update app.py

--- app_CM/parteGiovanni/app.py
+++ app_CM/parteGiovanni/app.py
@@ -1,5 +1,8 @@
 
 
+import re
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL, MySQLdb
 
@@ -54,9 +57,82 @@ def registro():
 		mysql.connection.commit()
 		return redirect(url_for('login'))
 
+
 @app.route('/principal', methods = ['GET' ,'POST'])
 def principal():
 	return render_template('principal.html')
 
-if __name__ == '__main__':
+
+@app.route('/fuentes', methods= ['GET' ,'POST'])
+def fuentes():
+	return render_template('fuentes.html')
+
+
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+
+@app.route('/preguntasJuntas/<enfermedad>/', methods=['POST', 'GET'])
+def resultJunto(enfermedad):
+	
+	file = open(f"static/preguntas_{enfermedad}.txt", encoding="utf-8")
+	lineas = file.readlines()
+	file.close()
+
+	if( request.method == 'POST' ):
+
+		resultado = {}
+		
+		for id in range(len(lineas)):
+
+			if id not in resultado:
+				resultado[id] = []
+			
+			respuestaCorrecta_id = request.form.get("correcta-"+str(id))
+			respuestaSelecta_id = request.form.get("respuesta-"+str(id))
+
+			if( respuestaSelecta_id == None ):
+				return redirect("/preguntasJuntas/"+enfermedad+"/")
+
+			if( respuestaSelecta_id == respuestaCorrecta_id ):
+				resultado[id].append(True)
+			else:
+				resultado[id].append(False)
+
+		# Aqui es donde guardariamos la información en base de datos
+		print(resultado)
+		return str(resultado)
+
+	# Supongamos los archivos estan en /static/
+	# y se llama usuario_NOMBRE.txt
+	# y su formato es por fila es:
+	# 	<PREGUNTA>|<RESPUESTA>|<RESPUESTA>|<RESPUESTA>|<RESPUESTA>|<INDICE_RESPUESTA_CORRECTA>
+	#ej:¿Estas Triste?|Si|No|Tal vez|No lo se|1
+
+	preguntas = []
+	id = 0
+	for linea in lineas:
+		data = linea.strip().split("|")
+		pregunta = data[0] #Pregunta
+		respuestas = data[1:-1] #Respuestas
+		correcta = data[-1] #Indice de la respuesta correcta
+		
+		# Creamos un diccionario con la pregunta y sus respuestas
+		dic = {
+			"id": id, 
+			"pregunta": pregunta, 
+			"respuestas": respuestas, 
+			"correcta": correcta
+			}
+
+		# Añadimos el diccionario al array de preguntas
+		preguntas.append(dic)
+		id+=1
+
+	return render_template("preguntasJuntas.html", preguntas = preguntas)
+
+
+if __name__ == "__main__":
 	app.run(debug=True)
